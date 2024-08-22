2d1b842705995a4e58896023861a05487346ad11
AnaClaraprisma <an4claracouto@gmail.com>
2024-08-22 16:31:19 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,42 @@
+//1.
+function soma(a, b) {
+    return a + b
+}
+console.log(`o resultado é :${soma(2, 3)}`);
+
+
+//2.
+function subtrair(a, b) {
+    return a - b
+}
+console.log(`o resuldado é :${subtrair(20, 3)}`);
+
+
+//3.
+function multiplicar(a, b) {
+    return a * b
+}
+console.log(`o resuldado é :${multiplicar(20, 3)}`);
+
+
+//4.
+function dividir(a, b) {
+
+    if (b % 2 == 0) {
+      console.log(a/b) 
+
+    }
+    else {
+        console.log(`b = ${b}`)
+    }
+    console.log(`o resuldado é :${dividir(20, 4)}`);
+}
+
+
+
+//5.
+function potencia (a, b){
+    return 
+
+}
+console.log(`o resuldado é :${potencia(2, 7)}`);
